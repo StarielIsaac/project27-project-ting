d1c5e5e8877253b54fafe87a092f86cc86c15215
Stariel Isaac <starielisaac@gmail.com>
2023-06-15 16:24:38 -0300

    feat: implemented  read files in def txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -4,9 +4,15 @@ import sys
 
 def txt_importer(path_file):
     file = Path(path_file)
-    if not file.endswith(".txt"):
+    if file.suffix != ".txt":
+        sys.stderr.write("Formato inválido\n")
         return None
 
     if not file.exists():
-        print(f"Arquivo {file} não encontrado", file=sys.stderr)
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
         return None
+
+    with open(path_file, "r") as file:
+        lines = [line.rstrip() for line in file]
+
+    return lines
