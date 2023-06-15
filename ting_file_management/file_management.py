4e309fa40f3f16b66e78170ea9ecb64de0a15eee
Stariel Isaac <starielisaac@gmail.com>
2023-06-15 16:53:42 -0300

    feat: implemented def process

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -4,14 +4,18 @@ import sys
 
 def txt_importer(path_file):
     file = Path(path_file)
+
+    # Verifica se o formato do arquivo é válido
     if file.suffix != ".txt":
         sys.stderr.write("Formato inválido\n")
         return None
 
+    # Verifica se o arquivo existe
     if not file.exists():
         sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
         return None
 
+    # Lê as linhas do arquivo
     with open(path_file, "r") as file:
         lines = [line.rstrip() for line in file]
 
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,8 +1,28 @@
 from ting_file_management.file_management import txt_importer
-import sys
+from ting_file_management.queue import Queue
 
-def process(path_file, instance):
-    
+
+def process(path_file, instance: Queue):
+    # Verifica se o arquivo já está na instância
+    for file in instance._data:
+        if file["nome_do_arquivo"] == path_file:
+            return None
+
+    # Lê as linhas do arquivo
+    lines = txt_importer(path_file)
+
+    # Cria um dicionário com as informações do arquivo
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+
+    # Adiciona o novo dicionário à instância
+    instance.enqueue(new_dict)
+
+    # Imprime o dicionário
+    print(new_dict)
 
 
 def remove(instance):
