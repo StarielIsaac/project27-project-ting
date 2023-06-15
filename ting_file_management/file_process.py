3bf26b84bbcdf6786fb8c98a18b4a37240b58142
Stariel Isaac <starielisaac@gmail.com>
2023-06-15 17:43:46 -0300

    docs: created comments in file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -27,19 +27,25 @@ def process(path_file, instance: Queue):
 
 
 def remove(instance: Queue):
-
+    # Verifica se há elementos na instância
     if len(instance._data) <= 0:
         print("Não há elementos")
         return None
 
+    # Remove o arquivo da instância e obtém o nome do arquivo removido
     removed = instance.dequeue()["nome_do_arquivo"]
 
+    # Imprime a mensagem de sucesso com o nome do arquivo removido
     print(f"Arquivo {removed} removido com sucesso")
 
 
 def file_metadata(instance: Queue, position: int):
     try:
+        # Busca o arquivo na posição especificada na instância
         file = instance.search(position)
+
+        # Imprime as informações do arquivo
         print(file)
     except IndexError:
+        # Se a posição for inválida, exibe uma mensagem de erro
         sys.stderr.write("Posição inválida\n")
